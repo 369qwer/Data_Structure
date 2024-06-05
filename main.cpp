0b52390284b49fe2f0daf3e6af34e25e9e57b4f9
369qwer <140569099+369qwer@users.noreply.github.com>
2024-06-05 21:44:22 +0800

    A brief description of changes

--- main.cpp
+++ main.cpp
@@ -1,40 +1,8 @@
-#include"include/SqList.h"
+ï»¿#include"include/SqList.h"
 #include"MyFun.h"
 #include<iostream>
 int main() {
-	SqList S;
-	int w,x;
-	InitList_Sq(S);
-	//²åÈëÔªËØ
-	ListInsert_Sq(S, 1, 5);
-	ListInsert_Sq(S, 2, 66);
-	ListInsert_Sq(S, 3, 555);
-
-	//std::cout << ListLength(S) << std::endl;
-	//std::cout << w << std::endl;
-	//std::cout << DestroyList_Sq(S)<<std::endl;
-	//GetElem_Sq(S, 1, w);
-	//ListDelete_Sq(S, 1, x);
-	//std::cout << x << std::endl;
-	//std::cout << ListEmpty_Sq(S)<<std::endl;	
-
-	//²âÊÔÇ°Çı¡¢ºó¼Ìº¯Êı
-	#if 0
-	PriorElem_Sq(S, 555, w);
-	std::cout <<w<<std::endl;
-	NextElem_Sq(S, 5, x);
-	std::cout << x << std::endl;
-	#endif
 	
-	//²âÊÔLocateElem_Sq²éÕÒµÚÒ»¸öÖµÓëeÂú×ãcompare£¨£©ÔªËØµÄÎ»Ğò
-	#if 0
-		std::cout<<LocateElem_Sq(S, 66, ComparisonFunction);
-	#endif 
-
-	//²âÊÔ ListTraverse_SqÒÀ´Îµ÷ÓÃvisit()
-	#if 0
-		ListTraverse_Sq(S, VisitFunction);
-	#endif // 1
 
 	return OK;
 }
